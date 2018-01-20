86221c89bab4061d79a19c22a6e203aafd46767d
waderwu <waderwu@163.com>
2018-01-21 00:33:38 +0800

    clear all

--- wsniff/wshark/hello.py
+++
@@ -1,6 +0,0 @@
-def test():
-    while True:
-        print('helllo')
-
-if __name__ == '__main__':
-    test()
--- wsniff/wshark/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^packet/(?P<id>[0-9]+)$', views.packet_detail, name='packet'),
-    url(r'^stream/(?P<stream_index>[0-9]+)$',views.stream, name='stream'),
-]
